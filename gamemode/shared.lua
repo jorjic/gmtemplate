fc977134bebb352fef88162039fe9a04c563c382
Jorji Costava <jorji@420blaze.it>
2017-06-19 18:22:31 -0500

    Revert "Initial commit"
    This reverts commit 0d1d52f9db902a7a93a12aacbbb87ec6f55da613.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,41 +1,46 @@
 AddCSLuaFile()
 
-GM.Name 		= "Zombie Panic"
-GM.Author 		= "Jorji Costava <STEAM_0:0:23932200>"
+GM.Name 		= ""
+GM.Author 		= ""
 GM.Email 		= ""
 GM.Website 		= ""
 
 function recursiveInclusion(scanDirectory)
+	--print("Searching: " .. scanDirectory .. "/")
 	local files, directories = file.Find(scanDirectory .. "/*", "LUA")
 
-
+	
 	--Recursively scan discovered directories
 	for _, v in pairs(directories) do
 		recursiveInclusion(scanDirectory .. "/" .. v)
 	end
-
+	
 	--Handle all discovered files
 	for _, v in pairs(files) do
+		--Chop off first two directories, to satisfy inclusion path requirement
 		local relativePath = string.gsub(scanDirectory .. "/" .. v, GM.FolderName .. "/gamemode/", "")
-
+		
 		if not (v == "shared.lua" or v == "init.lua" or v == "cl_init.lua") then
-
+		
 			if string.match( v, "^sv") then
+				--print("Found: " .. v)
 				include(relativePath)
 			elseif string.match( v, "^sh") then
+				--print("Found: " .. v)
 				include(relativePath)
-
+			
 				if SERVER then
 					AddCSLuaFile(relativePath)
 				end
 			elseif string.match( v, "^cl") then
+				--print("Found: " .. v)
 				AddCSLuaFile(relativePath)
-
+				
 				if CLIENT then
 					include(relativePath)
 				end
 			end
-
+			
 		end
 	end
 end
