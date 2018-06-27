2f24e112f25c0521fce5b41840f089600670b933
Jorji Costava <jorji@420blaze.it>
2018-06-27 11:18:31 -0500

    Fix typo

--- gamemode/extras/cl_flash_on_ready.lua
+++ gamemode/extras/cl_flash_on_ready.lua
@@ -1,8 +1,10 @@
--- Flashes the taskbar icon once the game has fully loaded, if the window does not have focus.
--- This works best on Windows, and is untested on OSX and Linux versions.
+--[[-------------------------------------------------------------------------
+Flashes the taskbar icon once the game has fully loaded, if the window does not have focus.
+This works best on Windows, and is untested on OSX and Linux versions.
+---------------------------------------------------------------------------]]--
 
 local function flashOnInitPostEntity()
-	if system.HasFocus() then
+	if not system.HasFocus() then
 		system.FlashWindow()
 	end
 end
