e9e520837f8884a2757e2d6c72c09fb1dfca2216
jorjic <jorji@420blaze.it>
2018-06-27 11:06:23 -0500

    Added flashing indicator code

---
+++ gamemode/extras/cl_flash_on_ready.lua
@@ -0,0 +1,9 @@
+-- Flashes the taskbar icon once the game has fully loaded, if the window does not have focus.
+-- This works best on Windows, and is untested on OSX and Linux versions.
+
+local function flashOnInitPostEntity()
+	if system.HasFocus() then
+		system.FlashWindow()
+	end
+end
+hook.Add( "InitPostEntity", "FlashOnGameLoad", flashOnInitPostEntity )
